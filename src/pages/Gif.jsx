e7eae84fb9f44f2247979af0a073fcca308aab1a
neerajkumarc <chaudharyneeraj91@gmail.com>
2023-02-26 11:28:00 +0530

    added download button

--- src/pages/Gif.jsx
+++ src/pages/Gif.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-const Gif = () => {
-    // const id = useParams()
-  return (
-    <div className=" max-w-2xl mx-auto m-4 flex flex-col justify-center text-white">
-    <p className='text-center text-4xl p-2 bg-red-400'>Under construction🚧</p>
-    </div>
-  )
-}
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import fileDownload from "js-file-download";
 
-export default Gif
+const Gif = () => {
+  const { id } = useParams();
+  console.log(id);
+  const [gif, setGif] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const API_KEY = "1mwpV1pXzxX9PWodaVTxBGCXpAsda5up";
+
+  useEffect(() => {
+    fetch(`https://api.giphy.com/v1/gifs/${id}?api_key=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.data);
+        setGif(data.data);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleDownload = (url, filename) => {
+    axios
+      .get(url, {
+        responseType: "blob",
+      })
+      .then((res) => {
+        fileDownload(res.data, filename);
+      });
+  };
+  return (
+    <>
+      {loading ? (
+        <p className="text-center text-2xl text-white">Loading...</p>
+      ) : (
+        <div className="max-w-2xl mx-auto m-4 text-white flex flex-col items-center gap-4">
+          <img
+            className="rounded-md w-96"
+            src={gif.images.original.url}
+            alt="gif"
+          />
+          <div className="flex gap-4 items-center">
+            <button
+              className="bg-green-500 px-4 p-2 w-36 font-bold rounded"
+              onClick={() => {
+                handleDownload(
+                  gif.images.original.url,
+                  `download${gif.id}.gif`
+                );
+              }}
+            >
+              Download
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Gif;
